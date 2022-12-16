44dcf370e42539f7ab6cb8a3f07dee16a5f69440
AlexKaliuzhnyi <118567944+AlexKaliuzhnyi@users.noreply.github.com>
2022-12-17 00:01:34 +0200

    Add files via upload

---
+++ FibonacciNumber.java
@@ -0,0 +1,17 @@
+package FibonacciNumber;
+
+public class FibonacciNumber {
+    public static void main(String[] args){
+        System.out.println(fibonacciNumber(100));
+    }
+        private static long fibonacciNumber ( int n){
+            long[] arr = new long[n + 1];
+            arr[0] = 1;
+            arr[1] = 1;
+            for (int i = 2; i <= n; i++)
+                arr[i] = arr[i - 1] + arr[i - 2];
+            return arr[n];
+        }
+
+    }
+
